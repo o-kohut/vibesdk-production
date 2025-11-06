1c554f8c013dde9ab3f651e1133f0bf61f11086a
Nazar Makar <nm@crowdin.ua>
2025-11-06 17:24:04 +0200

    refactor: update ID generation to use custom alphabet in generateNanoId function

--- worker/utils/idGenerator.ts
+++ worker/utils/idGenerator.ts
@@ -3,12 +3,15 @@
  * Simple wrapper around crypto.randomUUID() for consistent ID generation
  */
 
-import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
 
 export function generateId(): string {
     return crypto.randomUUID();
 }
 
 export function generateNanoId(): string {
+    // Using custom alphabet to exclude `_` character because Crowdin auth doesn't support it
+    // Original alphabet: https://github.com/ai/nanoid/blob/main/url-alphabet/index.js
+    const nanoid = customAlphabet('useandom-26T198340PX75pxJACKVERYMINDBUSHWOLFGQZbfghjklqvwyzrict', 21);
     return nanoid();
 }
